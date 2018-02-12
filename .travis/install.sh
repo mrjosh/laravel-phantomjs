6247a511d25165517636117b87f1b0e18b85ee7a
Alireza Josheghani <josheghani.dev@gmail.com>
2018-02-12 16:50:41 +0330

    Removed osx and windows travis test

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+# Install some custom requirements on Linux
+if [ $(phantomjs --version) != '2.1.1' ]; then
 
-    # Install some custom requirements on OS X
-    brew update
+    echo "Installing phantomjs ...";
 
-    brew install phantomjs
-
-    phantomjs --version
-
-elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-
-    # Install some custom requirements on Linux
-    if [ $(phantomjs --version) != '2.1.1' ]; then
-
-        rm -rf $PWD/travis_phantomjs;
-        mkdir -p $PWD/travis_phantomjs;
-    fi
-
-    if [ $(phantomjs --version) != '2.1.1' ]; then
-
-        wget https://assets.membergetmember.co/software/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2;
-    fi
-
-    if [ $(phantomjs --version) != '2.1.1' ]; then
-
-        tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs;
-    fi
-
-    export PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
-
-    phantomjs --version
-
-elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
-
-    echo "Ok dude! :||||"
+    rm -rf $PWD/travis_phantomjs;
+    mkdir -p $PWD/travis_phantomjs;
 fi
+
+if [ $(phantomjs --version) != '2.1.1' ]; then
+
+    wget https://assets.membergetmember.co/software/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2;
+fi
+
+if [ $(phantomjs --version) != '2.1.1' ]; then
+
+    tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs;
+fi
+
+export PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
+
+phantomjs --version
