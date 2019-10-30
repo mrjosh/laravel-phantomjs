463e6f61c30c5f83349020ebcd0d4f317fd05cd4
MrJosh <josheghani.dev@gmail.com>
2019-10-30 09:22:45 +0330

    Update php version in travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install some custom requirements on Linux
-if [ $(phantomjs --version) != '2.1.1' ]; then
+if [[ $(phantomjs --version) != '2.1.1' ]]; then
 
     echo "Installing phantomjs ...";
 
@@ -9,12 +9,12 @@ if [ $(phantomjs --version) != '2.1.1' ]; then
     mkdir -p $PWD/travis_phantomjs;
 fi
 
-if [ $(phantomjs --version) != '2.1.1' ]; then
+if [[ $(phantomjs --version) != '2.1.1' ]]; then
 
     wget https://assets.membergetmember.co/software/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2;
 fi
 
-if [ $(phantomjs --version) != '2.1.1' ]; then
+if [[ $(phantomjs --version) != '2.1.1' ]]; then
 
     tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs;
 fi
